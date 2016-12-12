29cf9488060c354618e57ccec9860aec4e0825ec
hmedal <hugh.medal@gmail.com>
2016-12-12 13:18:35 -0600

    A few small changes.

--- src/apo.py
+++ src/apo.py
@@ -1,6 +1,7 @@
-
-
-
+import argparse
 
 if __name__ == "__main__":
-    
+    parser = argparse.ArgumentParser(description='Read filenames.')
+    parser.add_argument('-d', '--dat', help='the data file', default = "data.txt")
+    args = parser.parse_args()
+    dataFile = args.dat
